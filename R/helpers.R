659216f11fbd41b2ba47c486fb5faf6f7c3ced2a
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-03-31 15:44:13 +0200

    more functions:

--- R/helpers.R
+++ R/helpers.R
@@ -1,5 +1,7 @@
-# Function to extract Water Holding Capacity
+# Helper functions to get data from WATCH-WFDEI + CRU + others
+# based on ingestr
 
+# Function to extract Water Holding Capacity
 extract_whc <- function(file, siteinfo){
 
   siteinfo <- siteinfo[, c("lon", "lat")] |>
@@ -14,4 +16,24 @@ extract_whc <- function(file, siteinfo){
   ) |>
     tibble::as_tibble() |>
     dplyr::rename(whc = layer)
+}
+
+# Define function to aggregate forcing over day of year
+aggregate_forcing_doy <- function(forcing){
+  forcing |>
+    dplyr::group_by(doy) |>
+    summarise(date = first(date),                # 2001 as symbolic date
+              temp = mean(temp, na.rm = TRUE),
+              rain = mean(rain, na.rm = TRUE),
+              vpd = mean(vpd, na.rm = TRUE),
+              ppfd = mean(ppfd, na.rm = TRUE),
+              snow = mean(snow, na.rm = TRUE),
+              co2 = mean(co2, na.rm = TRUE),
+              fapar = mean(fapar, na.rm = TRUE),
+              patm = mean(patm, na.rm = TRUE),
+              tmin = mean(tmin, na.rm = TRUE),
+              tmax = mean(tmax, na.rm = TRUE),
+              ccov = mean(ccov, na.rm = TRUE)
+    ) |>                               # already ungrouped
+    dplyr::slice(1:365)                          # ignore leap years
 }
